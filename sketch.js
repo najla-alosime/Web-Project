ecf05ad1b3dad4eb9f0570cfe8b6baaa38d9f01d
najla-alosime <67576405+najla-alosime@users.noreply.github.com>
2022-04-26 07:46:19 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,8 +17,8 @@ function preload() {
     probabilityThreshold: 0.95
   };
   soundClassifier = ml5.soundClassifier('SpeechCommands18w', options);
-  uImg = loadImage('unicorn.png');
-  tImg = loadImage('train.png');
+  uImg = loadImage('tom&&jerry.png');
+  tImg = loadImage('catch.png');
   bImg = loadImage('background.jpg');
 }
 
